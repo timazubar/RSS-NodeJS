d29abedcccc69547663688a0d9800b5ddb69eabd
timazubar <timazubar33@gmail.com>
2020-04-06 14:40:03 +0300

    feat: add routers to app.js, add get users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,33 @@
+// const User = require('./user.model');
+
+const users = [
+  {
+    id: '1',
+    name: 'Andrew',
+    login: 'AndyJohnes',
+    password: 'adasdas'
+  },
+  {
+    id: '2',
+    name: 'Troy',
+    login: 'Peters',
+    password: 'etrticxv1324'
+  },
+  {
+    id: '3',
+    name: 'Antony',
+    login: 'Miles',
+    password: 'adkasmo992r'
+  }
+];
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return users;
 };
+
+// const getUserById(id) {
+//   const users = await user.getAll();
+//   return users.find(u => u.id === id);
+// }
 
 module.exports = { getAll };
